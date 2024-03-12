dbe094e8f4ef1c3f6fc2d272a7226d75e31eca35
AsmaaEbrhimes <135524763+AsmaaEbrhimes@users.noreply.github.com>
2024-03-12 13:22:39 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     useEffect(() => {
         setTimeout(() => {
             setLoading(false);
-        }, 500);
+        }, 3500);
     }, []);
 
     const changeAr = () => {
